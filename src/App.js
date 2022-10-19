0fc70f0ccd332ebe7b109578895cac9712c4cb9c
user <gregorlitvinov@gmail.com>
2022-10-19 23:20:00 +0300

    testPropList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, {useState} from "react";
 import Header from "./components/Header/Header";
 import ClassCounter from './components/ClassCounter'
+import '../src/components/style/App.css'
+import PostItem from "./components/postItem";
 
 function App() {
   const [Value, setValue] = useState('text in input')
   
   return (
     <div className="App">
-    <ClassCounter/>
+      <PostItem post={{id: 1, title: 'Javascript', body: 'Discription' }}/>
+      <PostItem post={{id: 2, title: 'Javascript', body: 'Discription' }}/>
+      <PostItem post={{id: 3, title: 'Javascript', body: 'Discription' }}/>
+ 
+      
     </div>
+  
   );
 }
 

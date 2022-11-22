6b3dce668620d21125f7716974bd26bcbb8c1b77
user <gregorlitvinov@gmail.com>
2022-11-22 23:32:31 +0200

    first commit

--- src/components/UI/PostForm.jsx
+++ src/components/UI/PostForm.jsx
@@ -11,8 +11,10 @@ const PostForm = (props) => {
 		setPostName('')
 		setPostText('')
 	}
+	
 	return (
 		<form>
+			
 			<MyInput
 				value={postName}
 				onChange={(e) => {
@@ -28,6 +30,7 @@ const PostForm = (props) => {
 				type="text"
 				placeholder='Описание поста'/>
 			<MyButton onClick={addNewPost}>Создать Пост</MyButton>
+
 		</form>
 	);
 };
